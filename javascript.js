50bf3749f62e8447f5a58cdab9e1ec4dbbcd74d5
SanishKumar <18sanishkumar@gmail.com>
2023-09-13 23:43:55 +0530

    File Uploaded!

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <h2>JavaScript Arithmetic</h2>
+// <p>A typical arithmetic operation takes two numbers and produces a new number.</p>
+
+// <p id="demo"></p>
+
+// <script>
+// let x = 100 + 50;
+// document.getElementById("demo").innerHTML = x;
+// </script>
+
+// </body>
+// </html>
+
+
+// <!DOCTYPE html>
+// <html>
+// <body>
+
+// <h2>My First JavaScript</h2>
+
+// <button type="button"
+// onclick="document.getElementById('demo').innerHTML = Date()">
+// Click me to display Date and Time.</button>
+
+// <p id="demo"></p>
+
+// </body>
+// </html> 
+
+
+
+
+
+// var login="https://google.com";
+// var loggedin="File loaded Successfully.";
+// console.log(loggedin);
+
+// let apples=1;
+// const height="6'0\"";
+
+
+function addNumbers(num1,num2){
+    const total=num1+num2;
+    return total;
+}
+
+const addtotal= addNumbers(1,9);
